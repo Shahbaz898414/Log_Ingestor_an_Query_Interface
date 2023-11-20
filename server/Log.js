76806a6416b68c2cf14ce40bf31b87dbfdd5775f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-11-20 19:33:46 +0530

    1

---
+++ server/Log.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+
+const logSchema = new mongoose.Schema({
+  level: {
+    type:String,
+    require:true
+  },
+  message:{
+    type:String,
+    require:true
+  },
+  resourceId:{
+    type:String,
+    require:true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  traceID: {
+    type:String,
+    require:true
+  },
+  spanID: {
+    type:String,
+    require:true
+  },
+  commit: {
+    type:String,
+    require:true
+  },
+  metadata: {
+    parentResourceId: {
+      type:String,
+      require:true
+    },
+  },
+});
+
+
+
+
+const Log = mongoose.model('Log', logSchema);
+
+
+
+module.exports = Log;
---
+++ server/server.js
@@ -0,0 +1,57 @@
+// mongodb+srv://shahbaz898khan:<password>@cluster0.lodiowi.mongodb.net/?retryWrites=true&w=majority
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const Log = require('./Log'); // Adjust the path as needed
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const DB = "mongodb+srv://shahbaz898khan:npKFaYZoYLnMAzLx@cluster0.n5ovxnf.mongodb.net/?retryWrites=true&w=majority";
+// npKFaYZoYLnMAzLx
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=> {
+  console.log(`connection sucessful`);
+}).catch((err)=>console.log(err)
+)
+
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+
+app.post('/api/Log', (req, res) => {
+  const newLog = new Log(req.body);
+
+  newLog.save()
+    .then(() => res.json('Log added!'))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
+
+
+app.get('/',(req,res)=> {
+  res.send(`shabaz Hello world from the server`);
+});
+
+// console.log("shahabz")
+
+
+
+app.listen(3000,()=> {
+  console.log(`server is running at port no 3000`)
+})
+
